d1d8f56002ee06518c7a1c16cacbc379287bfef5
Chelosky <marcelolazo19972013@hotmail.com>
2021-02-21 05:21:25 -0300

    Footer First Iteration

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  statusApi: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
